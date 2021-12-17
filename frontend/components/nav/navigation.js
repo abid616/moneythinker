378cfab0e4f4b072a098f51332276883589fba38
simba0724 <bremaster0806@gmail.com>
2021-12-16 16:07:54 -0800

    navbar

--- frontend/components/nav/navigation.js
+++ frontend/components/nav/navigation.js
@@ -2,51 +2,45 @@ import React,{useState} from 'react';
 import Link from "next/link";
 import { v1 as uuidv1 } from 'uuid';
 import { FaAlignJustify, FaTimes } from "react-icons/fa";
+import { BiMenuAltRight } from "react-icons/bi";
 
 export const Navigation = ({categories}) => {
     const [showMenu, setShowMenu] = useState(false)
     const categoryList = () => {
         return(
-            <div>
+            <>
                 {categories.map((category, i)=>{
                     return(
                         <Link as={`/category/${category.slug}`} key={i} href="/category/[id]">
-                            <a
-                                key={i.name}
-                                className="text-sm md:text-lg uppercase font-bold text-gray-700 px-2 md:px-3 hover:bg-gray-300 rounded transition-colors duration-300"
-                            > {category.name} </a>
+                            <a key={i.name} className="text-sm md:text-lg uppercase font-bold text-gray-700 px-3 py-2 hover:bg-gray-300 rounded transition-colors duration-300"
+                            >
+                                {category.name}
+                            </a>
                         </Link>
                     )
                 })}
-            </div>
+            </>
         )
-        
     }
 
     let menu
     let menuMask
 
     if(showMenu){
-        menu = 
-            <div className="fixed bg-white top-14 left-0 w-4/5 h-5/12 z-50 shadow">
-                {categoryList()}
-            </div>
-        menuMask = 
-            <div
-            className="bg-black-t-50 fixed top-0 left-0 w-full h-full z-50"
-            onClick={() => setShowMenu(false)}
-            >
+        menu = <div className="fixed bg-white top-14 right-0 w-6/12 h-5/12 z-50 shadow flex flex-col lg:hidden">{categoryList()}</div>
 
-            </div>
+        menuMask = <div className="bg-black-t-50 fixed top-0 left-0 w-full h-full z-50 lg:hidden" onClick={() => setShowMenu(false)}></div>
     }
-
 
     return (
         <nav>
-            <div className="hidden md:block">
+            <div className="hidden lg:block">
                 {categoryList()}
             </div>
-            <FaAlignJustify className="md:hidden" onClick={() => setShowMenu(!showMenu)} />
+
+            {showMenu ? null : <BiMenuAltRight style={{fontSize: '25px'}} className="lg:hidden" onClick={() => setShowMenu(true)} />}
+            {showMenu ? <FaTimes style={{fontSize: '25px'}} className="lg:hidden" onClick={() => setShowMenu(false)} /> : null}
+
             {menuMask}
             {menu}
         </nav>
